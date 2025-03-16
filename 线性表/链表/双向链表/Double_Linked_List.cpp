a85bb3ce47739c91dad965edb955a28288adb24e
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-16 17:27:22 +0800

    Update Double_Linked_List.cpp

--- 线性表/链表/双向链表/Double_Linked_List.cpp
+++ 线性表/链表/双向链表/Double_Linked_List.cpp
@@ -9,7 +9,39 @@ typedef struct DuLNode
   struct DuLNode *next;
 }DuLNode,*DuLinkList;
 
-//双向链表的插入
+//双向链表的插入(带头结点)
 bool ListInsert_DuL(DuLinkList *L,int i,int e){
-  
+  DuLNode *p = *L;
+  int j = 0;
+  while(p-> next != NULL && j < i-1){
+    p = p->next;
+    j++;
+  }
+  if(p == NULL && j > i-1){
+    return false;
+  }
+  DuLNode *s = (DuLNode*)malloc(sizeof(DuLNoe));
+  s->next = p->next;
+  s->prior = p;
+  p->next->prior = s;
+  p->next = s;
+  s->data = e;
+  return true;
+}
+
+//双向链表的删除（带头结点）
+bool ListDelete_DuL(DuLinkList *L,int i){
+  DuLNode *p = *L;
+  int j = 0;
+  whil->next != NULL && j < i-1){
+    p = p->next;
+    j++;
+  }
+  if(p == NULL && j > i-1){
+    return false;
+  }
+  p->next = p->next->next;
+  p->next->next->prior = p;
+  free(p->next);
+  return true;
 }
