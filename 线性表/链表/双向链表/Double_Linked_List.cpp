658f6854def8a82c44492715745b8ce8adff2ca0
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-16 16:53:51 +0800

    Update Double_Linked_List.cpp

--- 线性表/链表/双向链表/Double_Linked_List.cpp
+++ 线性表/链表/双向链表/Double_Linked_List.cpp
@@ -1,4 +1,15 @@
 #include<stdio.h>
 #include <iostream>
 #include <stdlib.h>
-typedef struct  
+//仅需涉及一个方向的指针，双向链表的操作与单链表相同
+typedef struct DuLNode
+{
+  int data;
+  struct DuLNode *prior;
+  struct DuLNode *next;
+}DuLNode,*DuLinkList;
+
+//双向链表的插入
+bool ListInsert_DuL(DuLinkList *L,int i,int e){
+  
+}
