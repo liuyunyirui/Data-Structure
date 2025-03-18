323a9237d4e55a1b0e166c2c9e2c34bb05cafb02
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-18 20:32:16 +0800

    Update Linked Stack.cpp

--- 栈与队列/链栈/Linked Stack.cpp
+++ 栈与队列/链栈/Linked Stack.cpp
@@ -1,1 +1,20 @@
-#include<>
+#include<stdio.h>
+#include<stdlid.h>
+typedef struct{
+  int data;
+  struct StackNode *next; 
+}StackNode,*LinkStack;
+
+//初始化
+bool InitStack(LinkStack *S){
+  S = NULL;
+  return true;
+}
+
+//入栈
+bool Push(LinkStack *S)
+
+int main(){
+  StackNode S;
+  InitStack(&S);
+}
