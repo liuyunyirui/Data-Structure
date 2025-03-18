fee98be30037685ec07ca3de08562a39daee91fc
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-18 21:31:35 +0800

    Update Linked Stack.cpp

--- 栈与队列/链栈/Linked Stack.cpp
+++ 栈与队列/链栈/Linked Stack.cpp
@@ -3,18 +3,52 @@
 typedef struct{
   int data;
   struct StackNode *next; 
-}StackNode,*LinkStack;
+}StackNode,*LinkStack;//*LinkStack是一个结构指针
 
 //初始化
-bool InitStack(LinkStack *S){
+bool InitStack(LinkStack *S){//
   S = NULL;
   return true;
 }
 
 //入栈
-bool Push(LinkStack *S)
+bool Push(LinkStack *S,int e){
+  StackNode *p = (int*)malloc(sizeof(int));
+  p->data = e;
+  p->next = *S;
+  S = p;
+  return true; 
+}
+
+//出栈
+bool Pop(LinkStack *S,int *e){
+  if(S == NULL) return false;
+  StackNode *p = S;
+  *e = S->data;
+  S = S->next;
+  free(p);
+  return true;
+}
+
+//取栈顶元素
+int GetTop(LinkStack S){
+  if(S != NULL){
+    return S->data;
+  }
+}
+
+// 销毁链栈
+void DestroyStack(LinkStack* S) {
+    StackNode* current = *S;
+    while (current != NULL) {
+        StackNode* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    *S = NULL;  // 头指针置空
+}
 
 int main(){
-  StackNode S;
+  LinkStack S;
   InitStack(&S);
 }
