35c1af2ff96109bafdd406e857388fcf1bbe3f3b
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-22 20:33:20 +0800

    Update LinkQueue.cpp

--- 栈与队列/链队列/LinkQueue.cpp
+++ 栈与队列/链队列/LinkQueue.cpp
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct QNode{
+  int data;
+  struct QNode *next;
+}QNode,*QueuePtr;
+
+typedef struct{
+  QNode *front;
+  QNode *rear;
+}LinkQueue;
+
+//初始化
+bool InitQueue(LinkQueue *Q){
+  Q->front = Q->rear = (*QNode)malloc(sizeof(QNode));
+}
+
+
 
