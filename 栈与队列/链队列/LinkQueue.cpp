13170489820bfc76f9a3f317337847d3110a37a4
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-22 20:52:29 +0800

    Update LinkQueue.cpp

--- 栈与队列/链队列/LinkQueue.cpp
+++ 栈与队列/链队列/LinkQueue.cpp
@@ -10,10 +10,28 @@ typedef struct{
   QNode *rear;
 }LinkQueue;
 
-//初始化
+//初始化(带头结点)
 bool InitQueue(LinkQueue *Q){
-  Q->front = Q->rear = (*QNode)malloc(sizeof(QNode));
+  Q->front = Q->rear = (QNode*)malloc(sizeof(QNode));
+  Q->front->next = Q->rear->next = NULL;
+  return true;
 }
 
+//入队
+bool EnQueue(LinkQueue *Q,int e){
+  QNode *p = (QNode*)mallc(sizeof(QNode));
+  p->data = e;
+  p->next = NULL;
+  Q->rear->next = p;
+  Q->rear = p;
+  return true;
+}
+
+//出队
+
+int main(){
+  LinkQueue Q;
+  InitQueue(&Q);
+} 
 
 
