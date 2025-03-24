e1f18a93a5942bde1835b86745c22fcfc5122249
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-03-24 17:49:41 +0800

    Update LinkQueue.cpp

--- 栈与队列/链队列/LinkQueue.cpp
+++ 栈与队列/链队列/LinkQueue.cpp
@@ -19,7 +19,8 @@ bool InitQueue(LinkQueue *Q){
 
 //入队
 bool EnQueue(LinkQueue *Q,int e){
-  QNode *p = (QNode*)mallc(sizeof(QNode));
+  QNode *p = (QNode*)malloc(sizeof(QNode));
+  if(p == NULL) return false;
   p->data = e;
   p->next = NULL;
   Q->rear->next = p;
@@ -28,6 +29,40 @@ bool EnQueue(LinkQueue *Q,int e){
 }
 
 //出队
+bool DeQueue(LinkQueue *Q,int *e){
+  if(Q->front->next == NULL){
+    return false;
+  }
+  QNode *p = Q->front->next;
+  *e = p->data;
+  Q->front->next = p->next;
+  if(Q->rear == p){
+    Q->rear = Q->front;
+  }
+  free(p);
+  return true;
+} 
+ 
+//取队头元素
+bool GetHead(LinkQueue *Q,int *e){
+  if(Q->front->next != NULL){
+    *e = Q->front->next->data;
+    return true;
+  }
+  return false;
+}
+
+//销毁队列
+void DestroyQueue(LinkQueue *Q){
+  QNode *p = Q->front->next;
+  while(p != NULL){
+    QNode *temp = p;
+    p = p->next;
+    free(temp);
+  }
+  free(Q->front);
+  Q->front = Q->rear = NULL;
+}
 
 int main(){
   LinkQueue Q;
