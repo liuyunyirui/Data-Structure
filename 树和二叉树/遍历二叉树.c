6076e2f3eaf918f6d595c0170b4d569126a512d4
liuyunyirui <125025128+liuyunyirui@users.noreply.github.com>
2025-04-12 16:54:48 +0800

    Update 遍历二叉树.c

--- 树和二叉树/遍历二叉树.c
+++ 树和二叉树/遍历二叉树.c
@@ -1,1 +1,26 @@
+//先序遍历
+void PreOrder(BiTree T){
+  if(T != NULL){
+    visit(T);
+    PreOrder(T->lchild);
+    PreOrder(T->rchild);
+  }
+}
 
+//中序遍历
+void InOrder(BiTree T){
+  if(T != NULL){
+    InOrder(T->lchild);
+    visit(T);
+    InOrder(T->rchild);
+  }
+}
+
+//后序遍历
+void PostOrder(BiTree T){
+  if(T != NULL){
+    PostOrder(T->lchild);
+    PostOrder(T->rchile);
+    visit(T);
+  }
+}
